3c73a33a462880dcc18cd1d958bbbb49cb04a5e0
sergei <mikhajlov87@gmail.com>
2017-03-29 16:08:01 +0300

    header nav menu, add :href and :title

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+/*
+* @author S.Mikhajlov
+* */
 
+new Vue ({
+    el: "#menu",
+    data: {
+        links: [
+            {
+                name: 'About',
+                href: '/about',
+                title: 'About'
+            },
+
+            {
+                name: 'Education',
+                href: '/education',
+                title: 'Education'
+            },
+
+            {
+                name: 'Universities',
+                href: '/universities',
+                title: 'Университеты'
+            },
+
+            {
+                name: 'Services',
+                href: '/services',
+                title: 'Услуги'
+            },
+
+            {
+                name: 'Blog',
+                href: '/news',
+                title: 'Новости'
+            },
+
+            {
+                name: 'Contact',
+                href: '/contacts',
+                title: 'Контакты'
+            }
+        ]
+    }
+});
